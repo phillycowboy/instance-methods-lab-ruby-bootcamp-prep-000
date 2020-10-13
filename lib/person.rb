fa9cb20cb041600b2faf48bddd6ad4e735c2741b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-13 23:36:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,17 @@
 class Person
   def talk
     puts "Hello World!"
-  end
-end
-
-class Person
-  def walk
+      def walk
     puts "The Person is walking"
     
   end
 end
+  end
+end
+
+# class Person
+#   def walk
+#     puts "The Person is walking"
+    
+#   end
+# end
